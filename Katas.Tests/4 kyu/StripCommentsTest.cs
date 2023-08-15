d5e62033607d80bfecbbcf4f836652507188401e
Andrii Ochkurov <Andrii_Ochkurov@epam.com>
2023-08-15 13:44:57 +0300

    Init

---
+++ Katas.Tests/4 kyu/StripCommentsTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using NUnit.Framework;
+public class StripCommentsTest
+{
+    [Test]
+    public void StripComments()
+    {
+        Assert.AreEqual(
+                "apples, pears\ngrapes\nbananas",
+                StripCommentsSolution.StripComments("apples, pears # and bananas\ngrapes\nbananas !apples", new string[] { "#", "!" }));
+
+        Assert.AreEqual("a\nc\nd", StripCommentsSolution.StripComments("a #b\nc\nd $e f g", new string[] { "#", "$" }));
+
+    }
+}
---
+++ Katas/4 kyu/StripComments.cs
@@ -0,0 +1,26 @@
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System;
+using System.Text;
+
+public class StripCommentsSolution
+{
+    public static string StripComments(string input, string[] commentSymbols)
+    {
+        string[] lines = input.Split('\n');
+        for (int i = 0; i < lines.Length; i++)
+        {
+            foreach (string marker in commentSymbols)
+            {
+                int index = lines[i].IndexOf(marker);
+                if (index != -1)
+                {
+                    lines[i] = lines[i].Substring(0, index);
+                }
+            }
+            lines[i] = lines[i].TrimEnd();
+        }
+        return string.Join("\n", lines);
+    }
+}
