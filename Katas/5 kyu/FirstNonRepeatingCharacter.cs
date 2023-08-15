34a668e3311f4f9c3f85eee5bc0ff73c75f5ba1f
Andrew <andrey4016@gmail.com>
2023-08-15 14:07:00 +0300

    Added First non-repeating character

---
+++ Katas/5 kyu/FirstNonRepeatingCharacter.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+public class Kata
+{
+  public static string FirstNonRepeatingLetter(string s)
+  {
+    var sub = s.ToLower();
+    for (int i = 0; i < s.Length; i++)
+    {
+      if (!sub.Select((c, index) => new    
+      {    
+        StringChar = c,    
+        Position = index    
+        }).Where(item => item.StringChar.Equals(sub[i]) && item.Position != i).Any())
+      {
+        return s[i].ToString();
+      }
+    }
+
+    return string.Empty;
+  }
+}
