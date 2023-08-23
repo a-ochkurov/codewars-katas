7a24d8a3bd830327ce4108f6a73ae1bb8680bbd1
Andrew <andrey4016@gmail.com>
2023-08-23 15:01:09 +0300

    Refactor Moving Zeros To The End

--- Katas/5 kyu/MoveZeroes.cs
+++ Katas/5 kyu/MoveZeroes.cs
@@ -2,21 +2,7 @@
 {
     public static int[] MoveZeroes(int[] arr)
     {
-        var nonZero = new List<int>();
-        var zeroList = new List<int>();
-        foreach (int i in arr)
-        {
-            if (i == 0) {
-                zeroList.Add(0);
-            } else {
-                nonZero.Add(i);
-            } 
-        }
-        var res = new List<int>();
-        res.AddRange(nonZero);
-        res.AddRange(zeroList);
-        
-        return res.ToArray();
+        return arr.OrderBy(x => x==0).ToArray();
     }
 
 }
