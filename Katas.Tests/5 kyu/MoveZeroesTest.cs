26ab073a0e8b5ba89dce27c1cf1e4d7715d20974
Andrew <andrey4016@gmail.com>
2023-08-23 14:18:21 +0300

    Moving Zeros To The End

---
+++ Katas.Tests/5 kyu/MoveZeroesTest.cs
@@ -0,0 +1,15 @@
+﻿namespace Solution 
+{
+  using NUnit.Framework;
+  using System;
+
+  [TestFixture]
+  public class MoveZeroesTest
+  {
+    [Test]
+    public void Test()
+    {
+      Assert.AreEqual(new int[] {1, 2, 1, 1, 3, 1, 0, 0, 0, 0}, MoveZeroesClass.MoveZeroes(new int[] {1, 2, 0, 1, 0, 1, 0, 3, 0, 1}));
+    }
+  }
+}
